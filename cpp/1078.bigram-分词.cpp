89c5cc5d17a77c3c97928573d82b32cd72c8a7be
lmile <lvmax1998@163.com>
2021-12-29 21:52:17 +0800

    🎉 初次提交

---
+++ cpp/1078.bigram-分词.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=1078 lang=cpp
+ *
+ * [1078] Bigram 分词
+ */
+
+#include "head.h"
+
+// @lc code=start
+class Solution
+{
+public:
+    vector<string> findOcurrences(string text, string first, string second)
+    {
+        vector<string> words;
+        vector<string> result;
+
+        int begin = 0, end = 0;
+        while (end != -1)
+        {
+        }
+
+        words.push_back(text.substr(end));
+    }
+};
+// @lc code=end
