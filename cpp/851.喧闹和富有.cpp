23ce11cff63d2d5aba271bcf48715dbe184c51ac
lw <lvmax1998@163.com>
2022-01-10 16:20:33 +0800

    ❌ 不会

---
+++ cpp/851.喧闹和富有.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=851 lang=cpp
+ *
+ * [851] 喧闹和富有
+ */
+
+#include "head.h"
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> loudAndRich(vector<vector<int>>& richer, vector<int>& quiet) {
+        
+    }
+};
+// @lc code=end
+
